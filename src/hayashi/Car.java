b98eb4f3ace9630e3b5df712d7fd97f6d9ef5f58
watapon <watapon@192.168.0.12>
2017-08-24 19:48:24 +0900

    2017/8/24

--- src/hayashi/Car.java
+++ src/hayashi/Car.java
@@ -1,33 +1,73 @@
 package hayashi;
-import java.util.Scanner;
 
-class Jikyu{
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-	 int tanka,jikan;
+public class Car extends Frame{
 
-	public int kyuyo(){
-		return tanka * jikan;
-	}
+	private Button bt;
+	private TextField tf1,tf2;
+	private Label lb1,lb2,lb3;
 
-	public int zei(){
-		return (int)(kyuyo() * 0.1);
-	}
-
-	public int jyukyu(){
-		return (int)(kyuyo() - zei());
-	}
-}
-class Sample1{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-
-		Jikyu jikyu = new Jikyu();
-		jikyu.tanka = sc.nextInt();
-		jikyu.jikan = sc.nextInt();
-		System.out.println("時給：" + jikyu.tanka);
-		System.out.println("時間：" + jikyu.jikan);
-		System.out.println("給与：" + jikyu.kyuyo());
-		System.out.println("所得税：" + jikyu.zei());
-		System.out.println("支給額：" + jikyu.jyukyu());
+		Car c =new Car();
 	}
+
+	public Car(){
+		super("足し算");
+		setSize(300, 200);
+		setVisible(true);
+		setLayout(null);
+		tf1 = new TextField();
+		tf1.setBounds(20, 100, 50, 20);
+		add(tf1);
+		lb1 = new Label("+");
+		lb1.setBounds(80, 100, 10, 20);
+		add(lb1);
+		tf2 = new TextField();
+		tf2.setBounds(100, 100, 50, 20);
+		add(tf2);
+		lb2 = new Label("=");
+		lb2.setBounds(160, 100, 10, 20);
+		add(lb2);
+		lb3 = new Label("??");
+		lb3.setBounds(180, 100, 50, 20);
+		add(lb3);
+		bt = new Button("計算");
+		bt.setBounds(240, 100, 50, 20);
+		add(bt);
+		addWindowListener(new SampleWindowListener());
+		bt.addMouseListener(new SampleMouseListener());
+	}
+
+	class SampleWindowListener extends WindowAdapter{
+		public void windowClosing(WindowEvent e){
+			System.exit(0);
+		}
+	}
+	class SampleMouseListener extends MouseAdapter{
+		public boolean chkNum(String str){
+			try{
+				Double.parseDouble(str);
+				return true;
+			}catch(Exception e){
+				return false;
+			}
+		}
+		public void mouseClicked(MouseEvent e){
+			if(chkNum(tf1.getText()) && chkNum(tf2.getText())){
+				lb3.setText(String.valueOf(Double.parseDouble(tf1.getText())
+						+ Double.parseDouble(tf2.getText())));
+			}else{
+				lb3.setText("error");
+			}
+		}
+	}
+
 }
