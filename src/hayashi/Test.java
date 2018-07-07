fa3feb4792f81f4c00836043cf2fbd85adf52bd4
watapon <watapon@192.168.43.160>
2018-07-07 15:24:45 +0900

    2018/7/7

--- src/hayashi/Test.java
+++ src/hayashi/Test.java
@@ -4,5 +4,17 @@ public class Test{
 
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
+	    String s = sc.next();
+	    try {
+		if(Integer.parseInt(s) == 0 || s.equals(null)) {
+			System.out.println(s);
+		    }
+		    if(Integer.parseInt(s) == 2) {
+			System.out.println(s);
+		    }
+	    }catch(NullPointerException n) {
+		n.printStackTrace();
+	    }
+	    System.out.println("Hello");
 	}
 }
